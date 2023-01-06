db4849f0796c6b690a335cb73cdf6dea23ddb4d1
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-01-06 08:40:29 +0100

    first push

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <div className="border border-slate-300 bg-slate-100 w-full py-2 px-4">
+      header
+    </div>
+  );
+};
+
+export default header;
