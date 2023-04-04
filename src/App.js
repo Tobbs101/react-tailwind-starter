5cb3ea8e0641f2655624c31258f03671b17b6855
Tobi Idowu <97598133+Tobbs101@users.noreply.github.com>
2023-04-04 06:04:28 +0100

    Updated codebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Login from "./pages/SignIn";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Header from "./components/header/Header";
 
 function App() {
   return (
@@ -11,12 +6,7 @@ function App() {
       className="p-5"
       style={{ backgroundColor: "#f3f5f9", minHeight: "100vh" }}
     >
-      <Header />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <h1>Hello world</h1>
     </div>
   );
 }
